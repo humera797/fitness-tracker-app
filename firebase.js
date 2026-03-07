3814c20920fdec7c4ee4fe4b08c03e09440adc57
humera797 <humerasaeed419@gmail.com>
2026-03-08 00:20:16 +0500

    Configured Firebase for the app

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+// Firebase configuration and initialization for the App
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
