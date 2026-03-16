a8052c742240d1fb2d1a6876d880dbdce7274d1e
humera797 <humerasaeed419@gmail.com>
2026-03-16 23:45:34 +0500

    Import workout screen in App.js and added bottom tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -8,8 +9,10 @@ import SignupScreen from './screens/SignupScreen';
 import UserDataScreen1 from './screens/UserDataScreen1';
 import UserDataScreen2 from './screens/UserDataScreen2';
 import HomeScreen from './screens/HomeScreen';
+import WorkoutScreen from './screens/WorkoutScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -20,8 +23,42 @@ export default function App() {
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="UserData1" component={UserDataScreen1} />
         <Stack.Screen name="UserData2" component={UserDataScreen2} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+        tabLabelStyle: styles.tabLabel,
+        tabBarActiveTintColor: '#ffffff',
+        tabBarInactiveTintColor: '#655754',
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen}
+        options={{ tabBarLabel: 'Home ' }} />
+      <Tab.Screen name="Workout" component={WorkoutScreen}
+        options={{ tabBarLabel: 'Workout ' }} />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#C2B2AE',
+    borderTopWidth: 0,
+    height: 62,
+    position: 'absolute',
+  },
+  tabLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+  },
+});
+
+
